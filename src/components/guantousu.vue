559ee6823442ffabb0c3b66899e6d2e3bfb58947
snivy2 <39323455+snivy2@users.noreply.github.com>
2019-04-16 16:14:25 +0800

    dierci

--- src/components/guantousu.vue
+++ src/components/guantousu.vue
@@ -1,22 +1,134 @@
 <template>
   <div id="app">
-    <div >
-<h1>zheshiyonghutousu</h1>
+    <div  >
+
+ <el-table
+    :data="tousuArr"
+    style="width: 100%">
+    <el-table-column type="expand">
+      <template slot-scope="props">
+        <el-form label-position="left" inline class="demo-table-expand">
+          <el-form-item label="留言人">
+            <span class="neirong">{{ props.row.tousuName }}</span>
+          </el-form-item>
+          <el-form-item label="留言类型">
+            <span class="neirong">{{ props.row.tousuType }}</span>
+          </el-form-item>
+          <el-form-item label="联系地址">
+            <span class="neirong">{{ props.row.tousuAdd }}</span>
+          </el-form-item>
+          <el-form-item label="联系方式">
+            <span class="neirong">{{ props.row.phoneNum }}</span>
+          </el-form-item>
+          <el-form-item label="留言内容">
+            <span class="shangyi">{{ props.row.neirong }}</span>
+          </el-form-item>
+        </el-form>
+      </template>
+    </el-table-column>
+    <el-table-column
+      label="留言人"
+      prop="tousuName">
+    </el-table-column>
+    <el-table-column
+      label="类型"
+      prop="tousuType">
+    </el-table-column>
+    <el-table-column
+      label="联系方式"
+      prop="phoneNum">
+    </el-table-column>
+     <el-table-column
+      fixed="right"
+      label="操作"
+      width="120">
+      <template slot-scope="scope">
+        <el-button
+         @click="handleDelete(scope.$index, scope.row)"
+          type="text"
+          size="small">
+          已处理
+        </el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+
+
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: "app",
 
   data() {
     return {
+      tousuArr: []
+        ,
       msg: "Welcome to Your Vue.js App"
     };
   }
+  ,
+  created(){
+this.getApi();
+}
+,
+methods:{
+  getApi(){
+      axios.get('/api/tousu/xjh').then((res)=>{
+        this.tousuArr = res.data.result;
+      }).catch((err)=>{
+        console.log(err);
+      })
+    },
+     handleDelete(index, row) {
+       var obj = {
+        tousuName:row.tousuName
+      }
+      axios.post('/api/tousu/del',obj).then((res)=>{
+        if(res.data.status === '401'){
+          alert('不存在')
+        } else {
+          alert('处理成功');
+          // splice 可以添加删除数组中的元素
+          this.tousuArr.splice(index,1);
+        }
+      }).catch((err)=>{
+        alert('处理失败')
+      })
+      }
+}
 };
 </script>
 
 <style>
+.el-main[data-v-76ca982b] {
+    background-color: #ffffffff;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+}
+.neirong{
+display:block;
+padding-top:11px
+}
+.shangyi{
+  display: block;
+  padding-bottom: 70px
+}
+.demo-table-expand {
+    font-size: 0;
+  }
+  .demo-table-expand label {
+    width: 90px;
+    color: #99a9bf;
+  }
+  .demo-table-expand .el-form-item {
+    margin-right: 0;
+    margin-bottom: 0;
+    width: 50%;
+  }
+
 </style>
--- src/components/usertousu.vue
+++ src/components/usertousu.vue
@@ -1,22 +1,92 @@
 <template>
-  <div id="app">
-    <div >
+  <div id="app" class="wai">
+    <div class="tousu" >
+<el-form ref="form" :model="tousu" label-width="80px">
+  <el-form-item label="您的姓名">
+    <div class="baoxiuren">
+    <el-input v-model="tousu.tousuName"></el-input>
+    </div>
+  </el-form-item>
+    <el-form-item label="联系方式">
+    <div class="baoxiuren">
+    <el-input v-model="tousu.phoneNum"></el-input>
+    </div>
+  </el-form-item>
+    <el-form-item label="联系地址">
+    <div class="baoxiuren">
+    <el-input v-model="tousu.tousuAdd" placeholder="示例：5栋508"></el-input>
+    </div>
+  </el-form-item>
+  <el-form-item label="投诉/留言">
+    <div class="baoxiuleixin">
+    <el-select v-model="tousu.tousuType" placeholder="投诉/留言">
+      <el-option label="投诉" value="投诉"></el-option>
+      <el-option label="留言" value="留言"></el-option>
+    </el-select>
+    </div>
+  </el-form-item>
+
+  <el-form-item label="投诉内容">
+    <el-input type="textarea" v-model="tousu.neirong"></el-input>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" @click="onSubmit">提交</el-button>
+    <el-button>取消</el-button>
+  </el-form-item>
+</el-form>
+
 
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: "app",
 
   data() {
     return {
+       tousuArr:[],
+       tousu: {
+          tousuName: '',
+          tousuType: '',
+          phoneNum: '',
+          tousuAdd: '',
+          neirong:  ''
+        },
       msg: "Welcome to Your Vue.js App"
     };
+  }
+,
+  methods:{
+    onSubmit() {
+       axios.post('/api/tousu/add',this.tousu).then((res)=>{
+        if(res.data.status === '401'){
+          alert('投诉或留言已存在')
+        } else {
+          alert('添加成功');
+          this.tousuArr.push(this.tousu)
+        }
+      }).catch((err)=>{
+        alert('添加失败')
+      })
+    },
   }
 };
 </script>
 
-<style>
+<style >
+
+.tousu{
+  padding-top: 40px;
+  padding-right: 60px
+}
+.baoxiuren{
+  width:200px
+}
+.baoxiuleixin{
+  padding-left: -100px;
+  width:200px
+}
 </style>
